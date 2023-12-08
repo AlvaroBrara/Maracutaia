2ed5b7cb605d84ee895e283362d6ae05334d67ee
FreudZero <146487615+FreudZero@users.noreply.github.com>
2023-12-08 09:40:46 -0300

    Navios.py

--- Navios.py
+++ Navios.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 from Berços import *
+
 from Interface import *
 class navio(pygame.sprite.Sprite):
     def __init__(self, x_chegada, y_chegada, tipo, ):
@@ -8,35 +9,88 @@ class navio(pygame.sprite.Sprite):
         # Carregue as imagens dos navios
         if tipo == "carvao":
             self.image = pygame.image.load("navio_carvao.png")
-            self.tempo_descarga_inicial = 3
-            self.tempo_descarga = 3
+            self.tempo_descarga_inicial = 10
+            self.tempo_descarga = 5
             self.tempo_de_espera_inicial = 10 #tolerancia fixa
             self.tempo_de_espera = 10 #tolerancia que muda com o tempo passado
+            self.ponto_carvao= 10
         elif tipo == "soda_caustica":
             self.image = pygame.image.load("navio_soda_caustica.png")
-            self.tempo_descarga_inicial = 3
-            self.tempo_descarga = 3
+            self.tempo_descarga_inicial = 10
+            self.tempo_descarga = 5
             self.tempo_de_espera = 5
             self.tempo_de_espera_inicial = 5
+            self.ponto_soda= 10
         elif tipo == "oleo_combustivel":
             self.image = pygame.image.load("navio_oleo_combustivel.png")
-            self.tempo_descarga_inicial = 3
-            self.tempo_descarga = 3
+            self.tempo_descarga_inicial = 10
+            self.tempo_descarga = 5
             self.tempo_de_espera = 7
             self.tempo_de_espera_inicial = 7
+            self.ponto_oleo=15
 
 
         self.image = pygame.transform.scale(self.image, (80, 100))  # Ajuste o tamanho conforme necessário
         self.rect = self.image.get_rect()
         self.rect.center = (x_chegada, y_chegada)
         self.cargo_tipo = tipo
-        # self.velocidade =2
+        self.chegou_destino=False
+        self.destino_atual_index = 0
+        self.pontos=0
+        self.pontos_contados=False
+
 tipo=['carvao', 'soda_caustica','oleo_combustivel']
 y_chegada = 450
+pontuacao=0
 navio_group = pygame.sprite.Group()
-navio_esperando = navio_group.copy()
-navios_em_porto= pygame.sprite.Group()
-velocidade_navio = 2
+for navio in navio_group:
+    navio_esperando = navio.copy()
+
+#tem que ajeitar a movimentação bugada e teleportar o navio diretamente para a chegada
+def Movimentar_Navios():
+    global x_chegada, y_chegada, posicoes_de_chegada
+
+    for i, navio in enumerate(navio_group): #compara nº_navios com o numero de destinos disponíveis
+        i = i % len(destino)
+        destino_x, destino_y = destino[i]
+        atual_x, atual_y = navio.rect.center
+        dx = destino_x - atual_x
+        dy = destino_y - atual_y
+        distancia = ((dx ** 2) + (dy ** 2)) ** 0.5
+        #mova o navio quando...
+        if distancia >=1:
+            move_x = dx / distancia * navio_velocidade
+            move_y = dy / distancia * navio_velocidade
+           # Verifique a colisão com o berço atual e desative o movimento se houver colisão
+            colisao_com_berco = False
+            for j, berco in enumerate(bercos_group):
+                if navio.rect.colliderect(berco.rect):
+                    if not bercos_ocupados[j]:
+                        bercos_ocupados[j] = True  # Marque o berço como ocupado
+                    else:
+                        colisao_com_berco = True
+                    break
+            if not colisao_com_berco:
+                new_x = atual_x + move_x
+                new_y = atual_y + move_y
+                navio.rect.center = (new_x, new_y)
+        else:
+            navio.chegou_destino = True  # Marca o navio como tendo chegado ao destino
+            navio.destino_atual_index += 1  # Atualiza o índice do próximo destino
+
+
+destino = [(100, y_chegada), (300, y_chegada), (500, y_chegada), (700, y_chegada)]
+origem = [(850, 600), (1050, 600), (1250, 600), (1450, 600)]
+navio_velocidade = 2
+posicoes_de_inicio = origem.copy()
+destino_disponivel = {
+    (100, y_chegada): True,
+    (300, y_chegada): True,
+    (500, y_chegada): True,
+    (700, y_chegada): True
+}
+
+
 
 def criar_navio_aleatorio():
     global posicoes_de_inicio, navio_group, tipo
@@ -49,48 +103,9 @@ def criar_navio_aleatorio():
     navio_group.add(novo_navio)
 
 
-destinos_ocupados = [False, False, False, False]
-
-navio_velocidade = 2
-destino = [(100, y_chegada), (300, y_chegada), (500, y_chegada), (700, y_chegada)]
-origem = [(850, 600)]
-posicoes_de_inicio = origem.copy()
-
-def Movimentar_Navios():
-    global x_chegada, y_chegada, navio_velocidade, destinos_ocupados
-
-    for i, navio in enumerate(navio_group):
-        i = i % len(destino)
-        destino_x, destino_y = destino[i]
-        atual_x, atual_y = navio.rect.center
-        dx = destino_x - atual_x
-        dy = destino_y - atual_y
-        distancia = ((dx ** 2) + (dy ** 2)) ** 0.5
-
-        if distancia > 0:
-            if not destinos_ocupados[i]:
-                move_x = dx / distancia * navio_velocidade
-                move_y = dy / distancia * navio_velocidade
-
-                colisao_com_berco = False
-                for j, berco in enumerate(bercos_group):
-                    if navio.rect.colliderect(berco.rect):
-                        if not bercos_ocupados[j]:
-                            bercos_ocupados[j] = True
-                        else:
-                            colisao_com_berco = True
-                        break
-
-                if not colisao_com_berco:
-                    new_x = atual_x + move_x
-                    new_y = atual_y + move_y
-                    navio.rect.center = (new_x, new_y)
-        else:
-            destinos_ocupados[i] = True
-
 navio_selecionado = None
 def cliques(navio):
-    global navio_selecionado,destino
+    global navio_selecionado
 
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
@@ -117,5 +132,4 @@ def cliques(navio):
                 bercos_ocupados[i] = True  # Marque o berço como ocupado
                 navio_selecionado.image.set_alpha(255)  # Restaura a transparência do navio ao movê-lo para um berço
                 navio_selecionado = None
-                destinos_ocupados[i] = False
                 break  # Saia do loop, pois o navio foi movido para um berço
