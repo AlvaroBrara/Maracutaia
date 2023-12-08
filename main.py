a4c970cbd7e20ce0ac911ed243d5b6ac81a6be3e
FreudZero <146487615+FreudZero@users.noreply.github.com>
2023-12-08 09:39:19 -0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import pygame
+import sys
 from Navios import *
+from Berços import *
+import pygame.font
 from Interface import*
 pygame.init()
 
@@ -9,9 +13,9 @@ tempo_maximo = 0
 tempo_para_proximo_navio = random.randint(tempo_minimo, tempo_maximo)
 
 # Loop principal do jogo
+pausa=False
 rodando = True
 while rodando:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando = False
@@ -19,42 +23,63 @@ while rodando:
             if event.button == 1:
                 for navio in navio_group:
                     cliques(navio)
+        elif event.type ==  pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                pausa = not pausa
 
-    tempo_atual += 1 / FPS
-    disponibilidade = destinos_ocupados.count(False)
-    if tempo_atual > tempo_para_proximo_navio:
-        if len(navio_group) != disponibilidade:
-            criar_navio_aleatorio()  # Chama a função para criar um novo navio
-            tempo_para_proximo_navio = tempo_atual + random.randint(tempo_minimo, tempo_maximo)  # Atualiza o tempo para o próximo navio
-    Movimentar_Navios()
-    navio_group.update()
+    if not pausa:
+        tempo_atual += 1 / FPS
 
-    tela.blit(background, (0, 0))
-    bercos_group.draw(tela)
-    navio_group.draw(tela)
+        venceu(pontuacao,0)  # 10 é o valor para vencer
+        perdeu(pontuacao, -100)  # -50 é o valor para perder
 
-    for navio in navio_group:
-        for destino_x, destino_y in destino:
-            if (navio.rect.centerx, navio.rect.centery) == (destino_x, destino_y):
-                Barra_Espera(navio)
 
-        for berco in bercos_group:
-            if navio.rect.colliderect(berco.rect):
-                Barra_Descarga(navio)
+        # inicializando o aparecimento e navios
+        if tempo_atual > tempo_para_proximo_navio:
+            if len(navio_group) != len(bercos_group):
+                criar_navio_aleatorio()  # Chama a função para criar um novo navio
+                tempo_para_proximo_navio = tempo_atual + random.randint(tempo_minimo, tempo_maximo)  # Atualiza o tempo para o próximo navio
 
-                # Atualize a tela
-                for i, berco in enumerate(bercos_group):
-                    if pygame.sprite.collide_rect(navio, berco):
-                        if not bercos_ocupados[i]:
-                            if navio.rect.center in posicoes_de_inicio:
-                                bercos_ocupados[i] = True
-                        if navio.tempo_descarga <= 0:
-                            navio_group.remove(navio)
-                            bercos_ocupados[i] = False
-    print(disponibilidade)
+        indicenavio = 0
+        Movimentar_Navios()
+        navio_group.update()
 
-    pygame.display.flip()
-    relogio.tick(FPS)
+        tela.blit(background, (0, 0))
+        bercos_group.draw(tela)
+        navio_group.draw(tela)
+
+        for navio in navio_group:
+            #print(navio_group)
+
+            for destino_x, destino_y in destino:
+                if (navio.rect.centerx, navio.rect.centery) == (destino_x, destino_y):
+                    Barra_Espera(navio,pontuacao, pausa)
+                    pontuacao = Barra_Espera(navio, pontuacao,pausa)
+
+
+
+            for berco in bercos_group:
+                if navio.rect.colliderect(berco.rect):
+                    Barra_Descarga(navio,pontuacao, pausa)
+                    pontuacao = Barra_Descarga(navio, pontuacao,pausa)
+
+                    # Atualize a tela
+                    for i, berco in enumerate(bercos_group):
+                        if pygame.sprite.collide_rect(navio, berco):
+                            if not bercos_ocupados[i]:
+                                if navio.rect.center in posicoes_de_inicio:
+                                    bercos_ocupados[i] = True #arranjar um jeito de tirar o navio e devolver ele pra formar fila.
+                            # Ajuste da espera e remoção do navio
+                            navio.tempo_descarga -= 0.01
+                            if navio.tempo_descarga <= 0:
+                                navio_group.remove(navio)
+                                bercos_ocupados[i] = False
+
+
+        renderizar_pontuacao(pontuacao)  # Renderiza a pontuação na tela
+        pygame.display.flip()
+        relogio.tick(FPS)
 # Finalização do Pygame e saída do programa
+pygame.mixer.music.stop()
 pygame.quit()
 sys.exit()
